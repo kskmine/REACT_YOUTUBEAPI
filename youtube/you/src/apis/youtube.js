14e82a7e7e3395d03c110343bfff0ddd52ea93cb
Emine <150101062@ogrenci.yalova.edu.tr>
2022-04-04 11:24:18 +0300

    Add files via upload

---
+++ youtube/you/src/apis/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const KEY = 'AIzaSyAe7SXfu4UxPClg7bdwYAB0ld7XK507qgk';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
